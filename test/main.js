09f73cf7603c28a0ce8ff10fba3185eee7b745f9
Sean Dokko <dokko1230@gmail.com>
2014-08-18 13:14:27 -0700

    Add some basic tests

--- test/main.js
+++ test/main.js
@@ -1,6 +1,34 @@
 
 describe('Main', function () {
-  it('expectation', function () {
-    expect(true).to.equal(true);
+
+  beforeEach(module('KeenTrackerModule'));
+
+  describe('keenTrackerService', function () {
+    var keenTrackerService, config;
+
+    beforeEach(inject(function($injector) {
+      keenTrackerService = $injector.get('keenTrackerService');
+
+      config = {
+        projectId: "",
+        writeKey: "",
+        readKey: ""
+      }
+    }));
+
+    it('should exist', function () {
+      expect(typeof keenTrackerService).toBe('object');
+    });
+
+    it('should have an initialize function', function () {
+      expect(typeof keenTrackerService.initialize).toBe('function');
+    });
+
+    it('should have an add function', function() {
+      expect(typeof keenTrackerService.add).toBe('function');
+    });
+
+    
   });
+
 });
